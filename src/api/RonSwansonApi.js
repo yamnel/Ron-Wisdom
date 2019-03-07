533bb50eb42f536aa97a04b9430a6e8d678f5f38
Yamnel <lokilindo@gmail.com>
2019-03-07 03:00:00 -0500

    Changed api call to https

--- src/api/RonSwansonApi.js
+++ src/api/RonSwansonApi.js
@@ -1,6 +1,6 @@
 export default class RonSwansonApi {
     static getQuote () {
-        return fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes')
+        return fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
             .then(response => response.json())
             .catch(err => err)
     }
